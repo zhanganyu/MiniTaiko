0b8303bbe25fb6e52af7bcdf32b1b921b80aa217
张安宇 <109327402@qq.com>
2018-04-05 02:36:14 +0800

    更新大地图

--- assets/Script/LazyLoad.js
+++ assets/Script/LazyLoad.js
@@ -32,24 +32,68 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        const mapBase = this.getComponent(cc.TiledMap).getLayer('基本地图');
-        //console.log(mapBase);
-
-        mapBase.node.setNodeDirty(true);
-
-        const bgMap = this.getComponent(cc.TiledMap).getLayer('地图底色');
+        
         //console.log(bgMap);
 
-        bgMap.node.setNodeDirty(true);
+        //bgMap.node.setNodeDirty(true);
+
+        
         
 
     },
 
     start () {
         
-        // tiles.forEach(tile => {
+        this.SCW = cc.director.getVisibleSize().width;
+        this.SCH = cc.director.getVisibleSize().height;
+        
+        //init word position 6587.73, 2366.17 
+        
+        this.node.position = cc.v2(-6587*this.node.scale, -2366*this.node.scale);
+        
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, function ( event ) {
+          
+          var delta = event.getDelta();
+          
+          console.log('touchDelta:' + delta.x +"width" + (this.node.width*this.node.scale - this.SCW));
+          
+          var posX = this.node.x + delta.x;
+          var posY = this.node.y + delta.y;
+          
+          console.log('posx:' + posX);
+          if (posX >=this.SCW - this.node.width*this.node.scale && posX <=0) {
+              this.node.x = posX;
+          }
+          
+          if (posY >=this.SCH - this.node.height*this.node.scale && posY <=0) {
+              this.node.y = posY;
+          }
+        },this);
+        
+        
+        this.node.on('mousedown', function ( event ) {
+          
+            var point = cc.v2(event.getLocation().x, event.getLocation().y);
+            point = this.node.convertToNodeSpace(point);
+            console.log('触摸坐标:' + event.getLocation().x +','+ event.getLocation().y);
+            console.log('pos after trans' + point);
+        },this);
+        
+        
+        
+        //控制缩放
+        this.scroll = 0.7;
+        
+        this.node.on(cc.Node.EventType.MOUSE_WHEEL, function(event) {
+            var s = event.getScrollY() > 0 ? 0.1 : -0.1;
+            //console.log('滚轮 ：' + s);
             
-        // });
+            this.scroll += s;
+            
+            this.scroll = cc.clampf(this.scroll, 0.1, 1);
+
+            this.node.scale = this.scroll;
+        }, this);
     },
 
     // update (dt) {},
